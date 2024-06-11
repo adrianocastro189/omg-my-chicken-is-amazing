7d5fc03f44b94aa2b56a90a1f71b06a7c7bab158
Adriano Castro <adrianostanley@gmail.com>
2024-06-11 09:42:03 -0300

    Update stormwind-library.lua

--- lib/stormwind-library.lua
+++ lib/stormwind-library.lua
@@ -2464,6 +2464,55 @@ local Container = {}
 -- end of Container
 
 --[[--
+The Item class is a model that maps game items and their properties.
+
+Just like any other model, it's used to standardize the way addons interact 
+with game objects, especially when item information is passed as a parameter
+to methods, events, datasets, etc.
+
+This model will grow over time as new expansions are released and new
+features are implemented in the library.
+
+@classmod Models.Item
+]]
+local Item = {}
+    Item.__index = Item
+    Item.__ = self
+    self:addClass('Item', Item)
+
+    --[[--
+    Item constructor.
+    ]]
+    function Item.__construct()
+        return setmetatable({}, Item)
+    end
+
+    --[[--
+    Sets the item id.
+
+    @tparam int value the item's id
+
+    @treturn Models.Item self
+    ]]
+    function Item:setId(value)
+        self.id = value
+        return self
+    end
+
+    --[[--
+    Sets the item name.
+
+    @tparam string value the item's name
+
+    @treturn Models.Item self
+    ]]
+    function Item:setName(value)
+        self.name = value
+        return self
+    end
+-- end of Item
+
+--[[--
 This model represents the group of all player containers condensed as a
 single concept.
 
@@ -2566,55 +2615,6 @@ if self.addon.inventory.track then
         self.playerInventory:mapContainers()
     end)
 end
-
---[[--
-The Item class is a model that maps game items and their properties.
-
-Just like any other model, it's used to standardize the way addons interact 
-with game objects, especially when item information is passed as a parameter
-to methods, events, datasets, etc.
-
-This model will grow over time as new expansions are released and new
-features are implemented in the library.
-
-@classmod Models.Item
-]]
-local Item = {}
-    Item.__index = Item
-    Item.__ = self
-    self:addClass('Item', Item)
-
-    --[[--
-    Item constructor.
-    ]]
-    function Item.__construct()
-        return setmetatable({}, Item)
-    end
-
-    --[[--
-    Sets the item id.
-
-    @tparam int value the item's id
-
-    @treturn Models.Item self
-    ]]
-    function Item:setId(value)
-        self.id = value
-        return self
-    end
-
-    --[[--
-    Sets the item name.
-
-    @tparam string value the item's name
-
-    @treturn Models.Item self
-    ]]
-    function Item:setName(value)
-        self.name = value
-        return self
-    end
--- end of Item
 
 --[[--
 The macro class maps macro information and allow in game macro updates.
