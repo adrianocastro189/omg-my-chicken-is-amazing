467eae6bb37280f66b4f7b8bf0053cb8c1db35c1
Adriano Castro <adrianostanley@gmail.com>
2024-07-09 14:12:49 -0300

    Add the Interactions lament methods

--- src/Interactions.lua
+++ src/Interactions.lua
@@ -8,10 +8,24 @@ local Interactions = {}
     Interactions.__index = Interactions
     AmazingChicken:addClass('Omg/Interactions', Interactions)
 
-    --[[--
+    --[[
     Interactions constructor.
     ]]
     function Interactions.__construct()
         return setmetatable({}, Interactions)
+    end
+
+    --[[
+    Laments the fact that the player's Westfall chicken is not summoned.
+    ]]
+    function Interactions:lamentChickenAbsence()
+        AmazingChicken.output:say('Oh, I wish my Westfall chicken was here with me...')
+    end
+
+    --[[
+    Laments the fact that the player doesn't have a Westfall chicken.
+    ]]
+    function Interactions:lamentLackOfChicken()
+        AmazingChicken.output:say('Oh, I wish I had a Westfall chicken...')
     end
 -- end of Interactions
--- tests/InteractionsTest.lua
+++ tests/InteractionsTest.lua
@@ -5,4 +5,28 @@ TestInteractions = BaseTestClass:new()
 
         lu.assertNotNil(instance)
     end
+
+    -- @covers Interactions:lamentChickenAbsence()
+    -- @covers Interactions:lamentLackOfChicken()
+    function TestInteractions:testLament()
+        local function execution(method, expectedMessageSaid)
+            local instance = AmazingChicken:new('Omg/Interactions')
+
+            AmazingChicken.output = {
+                say = function (self, message)
+                    AmazingChicken.messageArg = message
+                end
+            }
+
+            instance[method](instance)
+
+            lu.assertEquals(AmazingChicken.messageArg, expectedMessageSaid)
+        end
+
+        -- lament chicken absence
+        execution('lamentChickenAbsence', 'Oh, I wish my Westfall chicken was here with me...')
+
+        -- lament lack of chicken
+        execution('lamentLackOfChicken', 'Oh, I wish I had a Westfall chicken...')
+    end
 -- end of TestInteractions
